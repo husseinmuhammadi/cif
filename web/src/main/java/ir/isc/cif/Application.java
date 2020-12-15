b8d5ddefe6a0dc1718d5d9c1cf639355fef6ea6e
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2020-12-15 19:14:08 +0330

    CIF

---
+++ web/src/main/java/ir/isc/cif/Application.java
@@ -0,0 +1,11 @@
+package ir.isc.cif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
