71ddee7b59917e1425ed52c622c7c6fcca1d5dbb
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2020-12-20 17:41:47 +0330

    CIF Batch

--- repository/src/test/java/ir/isc/cif/TestApplication.java
+++ repository/src/test/java/ir/isc/cif/TestApplication.java
@@ -2,10 +2,11 @@ package ir.isc.cif;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootTest
 public class TestApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(TestApplication.class, args);
